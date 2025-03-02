c76eb6b31cd1220feaf58f9a61b598f2c5cdc043
tiemfah <sivanat.sub@gmail.com>
2025-03-02 14:06:55 +0100

    Remove reservationAPI spring

--- reservationAPI/src/main/java/io/swagger/exceptions/EventNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.swagger.exceptions;
-
-public class EventNotFoundException extends RuntimeException {
-    public EventNotFoundException(String message) {
-        super(message);
-    }
-}
